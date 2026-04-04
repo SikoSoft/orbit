53a98d2aaede7190ae8e247896082b3c1f6c0d68
Aaron Wright <aaron@sikosoft.com>
2026-04-04 09:18:16 +0200

    update vitest config so it reads tsconfig from tsconfig.eslint.json in order to fix test failure

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths({ projects: ['./tsconfig.eslint.json'] })],
   test: {
     environment: 'happy-dom',
     include: ['src/tests/unit/**/*.test.ts'],
