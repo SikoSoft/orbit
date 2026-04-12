a0fff9fadc6b5fb67c935ddd3d3df35125c61139
Aaron Wright <aaron@sikosoft.com>
2026-04-12 09:49:44 +0200

    update short-text-field component to handle limited option choices

--- src/components/entity-form/short-text-field/short-text-field.ts
+++ src/components/entity-form/short-text-field/short-text-field.ts
@@ -2,7 +2,11 @@ import { html, LitElement, TemplateResult } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 
 import '@ss/ui/components/ss-input';
-import { DataType } from 'api-spec/models/Entity';
+import {
+  DataType,
+  defaultEntityPropertyConfig,
+  EntityPropertyConfig,
+} from 'api-spec/models/Entity';
 import {
   InputChangedEvent,
   InputSubmittedEvent,
@@ -20,11 +24,19 @@ import {
 } from './short-text-field.models';
 import { SSInput } from '@ss/ui/components/ss-input';
 import { storage } from '@/lib/Storage';
+import { PropertyFieldProp } from '../property-field/property-field.models';
+import { MobxLitElement } from '@adobe/lit-mobx';
+import { appState } from '@/state';
+import { addToast } from '@/lib/Util';
+import { translate } from '@/lib/Localization';
+import { NotificationType } from '@ss/ui/components/notification-provider.models';
 
 const minLengthForSuggestion = 1;
 
 @customElement('short-text-field')
-export class ShortTextField extends LitElement {
+export class ShortTextField extends MobxLitElement {
+  public state = appState;
+
   @property({ type: Number })
   [ShortTextFieldProp.INSTANCE_ID]: ShortTextFieldProps[ShortTextFieldProp.INSTANCE_ID] =
     shortTextFieldProps[ShortTextFieldProp.INSTANCE_ID].default;
@@ -54,8 +66,31 @@ export class ShortTextField extends LitElement {
   @state()
   _value: string = '';
 
+  @state()
+  get propertyConfig(): EntityPropertyConfig {
+    let propertyConfig = defaultEntityPropertyConfig;
+
+    const entityConfig = this.state.entityConfigs.find(
+      config => config.id === this[PropertyFieldProp.ENTITY_CONFIG_ID],
+    );
+
+    if (entityConfig) {
+      propertyConfig =
+        entityConfig.properties.find(
+          prop => prop.id === this[PropertyFieldProp.PROPERTY_CONFIG_ID],
+        ) || defaultEntityPropertyConfig;
+    }
+
+    return propertyConfig;
+  }
+
   protected handleInputChanged(e: InputChangedEvent): void {
     const value = e.detail.value;
+
+    this.syncValue(value);
+  }
+
+  syncValue(value: string): void {
     this._value = value;
 
     const changedPayload: PropertyChangedEventPayload = {
@@ -84,13 +119,7 @@ export class ShortTextField extends LitElement {
       let suggestions: string[] = [];
 
       if (this._value.length >= minLengthForSuggestion) {
-        const result = await storage.getPropertySuggestions(
-          this[ShortTextFieldProp.PROPERTY_CONFIG_ID],
-          this._value,
-        );
-        if (result) {
-          suggestions = result;
-        }
+        suggestions = await this.getPropertySuggestions();
       }
 
       if (suggestions.length || this._value === '') {
@@ -105,10 +134,44 @@ export class ShortTextField extends LitElement {
     this.lastInput.value = this._value;
   }
 
+  async getPropertySuggestions(): Promise<string[]> {
+    if (this.propertyConfig.optionsOnly) {
+      return ([...this.propertyConfig.options] as string[]).filter(option =>
+        option.toLowerCase().startsWith(this._value.toLowerCase()),
+      );
+    }
+
+    let suggestions: string[] = [];
+    const result = await storage.getPropertySuggestions(
+      this[ShortTextFieldProp.PROPERTY_CONFIG_ID],
+      this._value,
+    );
+    if (result) {
+      suggestions = result;
+    }
+    return suggestions;
+  }
+
   handleInputSubmitted(_: InputSubmittedEvent): void {
     this.dispatchEvent(
       new PropertySubmittedEvent({ uiId: this[ShortTextFieldProp.UI_ID] }),
     );
+  }
+
+  handleInputBlurred(_: InputChangedEvent): void {
+    if (
+      this.propertyConfig.optionsOnly &&
+      !this.propertyConfig.options.includes(this._value)
+    ) {
+      addToast(
+        translate('propertyValueNotAllowed', {
+          value: this._value,
+          name: this.propertyConfig.name,
+        }),
+        NotificationType.ERROR,
+      );
+      this.syncValue('');
+    }
   }
 
   focus(): void {
@@ -122,6 +185,7 @@ export class ShortTextField extends LitElement {
     return html`
       <ss-input
         value=${this[ShortTextFieldProp.VALUE]}
+        @input-blurred=${this.handleInputBlurred}
         @input-changed=${this.handleInputChanged}
         @input-submitted=${this.handleInputSubmitted}
         .suggestions=${this.suggestions}
